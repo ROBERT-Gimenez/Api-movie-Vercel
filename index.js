56f8292814c13bd5e12fa49cd6bf6f19f2ad7075
robert giemenz <you@example.com>
2022-07-20 16:20:03 -0300

    config de mysql2 agregado

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const app = require("./src/app");
 const { get } = require("./src/routes");
 
@@ -6,5 +7,15 @@ const port =process.env.PORT || 4000;
 app.listen(port)
 
 
-app()
 console.log("listen on port"+ port)
+
+const mysql2 = require('mysql2');
+
+const config ={
+username:process.env.DATABASE_USERNAME,
+password:process.env.DATABASE_PASSWORD,
+database:process.env.DATABASE_NAME,
+host:process.env.DATABASE_HOST,
+dialect:"mysql",
+dialectModule:"mysql2"
+};
